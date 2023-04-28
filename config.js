27f9f97fb5587e09b6976c4e019ce9a9f9236bee
DOMINATOR-XD <113828214+DOMINATOR-XD@users.noreply.github.com>
2023-04-28 13:26:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -31,7 +31,7 @@ module.exports = {
 	HEROKU_APP_NAME: process.env.HEROKU_APP_NAME,
 	HEROKU_API_KEY: process.env.HEROKU_API_KEY,
 	BRANCH: 'master',
-	STICKER_PACKNAME: process.env.STICKER_PACKNAME || '❤️,LyFE',
+	STICKER_PACKNAME: process.env.STICKER_PACKNAME || '𝑰𝑻𝒁 𝑴𝑬 𝑺𝑶𝑼𝑳 𝑫𝑶𝑴𝑰𝑵𝑨𝑻𝑶𝑹❤️',
 	ALWAYS_ONLINE: toBool(process.env.ALWAYS_ONLINE),
 	LOG_MSG: toBool(process.env.LOG_MSG) || false,
 	RMBG_KEY: process.env.RMBG_KEY || 'null',
